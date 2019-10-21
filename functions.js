d058de42e59100a7a76db09e838096bdff9ea47d
Garrett James <garrettjames55@gmail.com>
2019-10-21 16:00:22 -0400

    added getDaysInMonth function

--- functions.js
+++ functions.js
@@ -40,6 +40,8 @@ function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
+jediName('Beyonce', 'Knowles');
+
 function beyond(num) {
   if (num === Infinity || num === -Infinity) {
     console.log(`And beyond`);
@@ -51,3 +53,82 @@ function beyond(num) {
     console.log(`Staying home`);
   }
 }
+
+beyond(5);
+beyond(0);
+beyond(6/0);
+
+function decode(word) {
+  if (word[0] === 'a') {
+    return word[1];
+  } else if (word[0] === 'b') {
+    return word[2];
+  } else if (word[0] === 'c') {
+    return word[3];
+  } else if (word[0] === 'd') {
+    return word[4];
+  } else {
+    return ' ';
+  }
+}
+
+decode();
+
+
+function getDaysInMonth(month, leapYear) {
+  switch(month) {
+  case 'january':
+    return `January has 31 days`;
+    break;
+  
+  case 'february':
+    if (leapYear) {
+      return `February has 29 days`;
+    } else {
+      return `February has 28 days`;
+    }
+    break;
+
+  case 'march':
+    return `March has 31 days`;
+    break;
+    
+  case 'april':
+    return `April has 30 days`;
+    break;
+        
+  case 'may':
+    return `May has 31 days`;
+    break; 
+    
+  case 'june':
+    return `June has 30 days`;
+    break;
+    
+  case 'july':
+    return 'July has 31 days';
+    break;
+    
+  case 'august':
+    return 'August has 31 days';
+    break;
+    
+  case 'september':
+    return `September has 30 days`;
+    break;
+    
+  case 'october':
+    return `October has 31 days`;
+    break;
+    
+  case 'november':
+    return `November has 30 days`;
+    break;
+    
+  case 'december':
+    return `December has 31 days`;
+    break;  
+  }  
+}
+
+getDaysInMonth('february', true);
