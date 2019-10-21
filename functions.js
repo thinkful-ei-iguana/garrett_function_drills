6646b2ff3e9856e71ea4a39ede2d3040e7985168
Garrett James <garrettjames55@gmail.com>
2019-10-21 14:27:25 -0400

    added error handling

--- functions.js
+++ functions.js
@@ -5,9 +5,18 @@ function getYearOfBirth(age) {
 }
 
 function createGreeting(name, age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
+
   const yearOfBirth = getYearOfBirth(age);
+  
   return `Hi my name is ${name} and I am ${age} years old and I was born in ${yearOfBirth}`;
 }
 
-const greeting = createGreeting('Garrett', 33);
-console.log(greeting);
+try {
+  const greeting = createGreeting('Garrett', 33);
+  console.log(greeting);
+} catch (e) {
+  console.log(e.message);
+}
