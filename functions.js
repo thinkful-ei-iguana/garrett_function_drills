aba6b51171b051f06213c488e2e0b7b961fd9ed0
Garrett James <garrettjames55@gmail.com>
2019-10-21 14:05:09 -0400

    updated createGreeting

--- functions.js
+++ functions.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi my name is ${name} and I am ${age} years old`;
+  const yearOfBirth = 2019 - age;
+  return `Hi my name is ${name} and I am ${age} years old and I was born in ${yearOfBirth}`;
 }
 
 const greeting = createGreeting('Garrett', 33);
