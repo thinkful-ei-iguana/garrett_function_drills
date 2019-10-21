5e927babba6fbace000d863fad65c4523c76ea68
Garrett James <garrettjames55@gmail.com>
2019-10-21 14:00:19 -0400

    updated createGreeting

--- functions.js
+++ functions.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting() {
-console.log('Hi my name is Garrett and I am 33 years old');
+  return 'Hi my name is Garrett and I am 33 years old';
 }
 
-createGreeting();
+const greeting = createGreeting();
+console.log(greeting);
