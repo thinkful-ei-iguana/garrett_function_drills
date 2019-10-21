c2afa6bf2e371e329bed1e08a1bae4963386c492
Garrett James <garrettjames55@gmail.com>
2019-10-21 15:08:33 -0400

    added beyond function

--- functions.js
+++ functions.js
@@ -39,3 +39,15 @@ try {
 function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
+
+function beyond(num) {
+  if (num === Infinity || num === -Infinity) {
+    console.log(`And beyond`);
+  } else if (isFinite(num) && num > 0) {
+    console.log(`To infinity`);
+  } else if(isFinite(num) && num < 0) {
+    console.log(`To negative infinity`);
+  } else {
+    console.log(`Staying home`);
+  }
+}
